9d44e61ce30b00344c36c8fca50dbf7049d3359b
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-07-26 20:49:21 +0530

    commit

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ class Evaluate:
   """
     # Write your code here
 
+
   def _init_(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
@@ -63,14 +64,14 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     # Write your code here
-    a = 0
-    b = 0
+    nums = 0
+    ops = 0
     for element in expression:
       if element.isnumeric():
-        a = a + 1
+        nums = nums + 1
       else:
-        b = b + 1
-    if b == a - 1:
+        ops = ops + 1
+    if ops == nums - 1:
       return True
     else:
       return False
