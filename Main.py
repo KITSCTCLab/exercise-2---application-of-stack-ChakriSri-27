576b886831423451ec3dff20ab2ca40c6d73bdfe
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-07-26 20:17:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ class Evaluate:
 
 
 
-  def __init__(self, size):
+  def _init_(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       top:An integer which points to the top most element in the stack.
